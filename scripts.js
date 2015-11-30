d7a8a7f3a0602a4ade8d9dfa869e81999929eef7
fitnesssite <naycrocodile@gmail.com>
2015-11-29 23:30:12 -0600

    T-Giving Updates
    Added lifestyle page, randomized fact generator, exercise page,
    stretching gallery (gym)

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 
 $(document).ready(function(){
 
-	
-$(window).scroll(function(){
-    $("#sidebar").css("top",Math.max(0,288-$(this).scrollTop()));
-});
-	
+
+
+/*function for scroll bar*/
+
+
+
+
+	/* sidebar animations */
 	$('#sidebar div').mouseenter(function(){
 	$(this).css('opacity', '1.0');
 	});
@@ -23,6 +26,8 @@ $(window).scroll(function(){
 	$("#d2").hover(function(){
 		$("#d2-a, #d2-b").show();
 	});
+	
+	 /* homepage grid animations */
 	 
 	$('#wall div').mouseenter(function(){
 		$(this).css('cursor', 'pointer');
@@ -38,4 +43,20 @@ $(window).scroll(function(){
 	$('#wall div').mouseleave(function(){
 		$(this).css('background-color', '');
 	});
+	
+	/* random log generator */
+	var stretchFact = ['Stretching exercises improve the flow of blood around your body. Improving your circulation means that more blood and oxygen are reaching your muscles and your brain. This can help avoid injuries in a workout and it’s also why it feels so good when we stretch first thing in the morning.', 'It may come as a surprise that stretching also helps patients with conditions such as diabetes and depression. In fact, recent studies by researchers at The Ohio State University Wexner Medical Center show that stretching during yoga classes can even benefit women who are battling breast cancer.', 'It is just as important, if not more so, to stretch after you exercise. Stretching while the muscles are already loose from a workout will help you recover faster and prevent injuries.', 'Women are generally more flexible than men of the same age.' ];
+	var rand = stretchFact[Math.floor(Math.random() * stretchFact.length)];
+	
+	document.getElementById('log').innerHTML += rand;
+	
+	
+	/* scrollify */
+	 $(document).ready(function() {
+        $('#fullpage').fullpage();
+    });
+	
+	
+	/* close tag */
   });
+  
